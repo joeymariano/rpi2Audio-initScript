127d121ae4c3b704901fd3e51bb50d5f9b817eea
Joey Michalina Mariano <joey.michalina.mariano@gmail.com>
2020-04-04 09:09:29 +0000

    reading file names and printing

--- initial_script.sh
+++ initial_script.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-mocp /home/pi/Music
+# mocp /home/pi/Music
+# uncomment for audio player first
 
+# vgmplayer on boot
 
+vgmplayer(){
+  cd /home/pi/Music/vgmSet
+  files=(*.vgm)
+
+  echo "files in /Music/vgmSet"
+
+  for i in ${files[@]}
+  do
+    echo $i
+  done
+}
+
+vgmplayer
