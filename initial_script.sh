c3ffb1ced5b0c9adf6741aed3857387b3d065746
Joey Michalina Mariano <joey.michalina.mariano@gmail.com>
2020-04-06 04:02:33 +0000

    added more colors

--- initial_script.sh
+++ initial_script.sh
@@ -7,44 +7,43 @@
 
 # vgmplayer on boot
 
-# change dir and setup variables
+# clear screen, change dir, and setup variables
+clear
 cd /home/pi/Music/vgmSet
 files=(*.vgm) # load all files into array
 count=0
 ans=""
 end=false
-
-# colors for echo
-LCYAN="\033[1;36m"
+LCYAN="\033[1;36m" # colors for echo
 LPURPLE="\033[1;35m"
 PURPLE="\033[0;35m"
 CYAN="\033[0;36m"
 LGREEN="\033[1;32m"
-NC="\033[0m"
+CYANBG="\033[46m"
+BLACK="\033[0;30m"
+NC="\033[0m" # no color
 
 # functions
 listTracks(){
   count=0
-  echo ""
-  echo -e "${PURPLE}files in ${LPURPLE}~/Music/vgmSet${NC}\n" # -e to allow color
+  echo -e "\n${PURPLE}files in ${LPURPLE}~/Music/vgmSet${NC}\n" # -e to allow color
 
   for filename in ${files[@]}
   do
     echo -e $CYAN$count$NC $LCYAN$filename$NC # clusterfuck but yea...
     ((count++))
   done
-
-  echo ""
 }
 
 selectTrack(){
-  echo -n -e "${PURPLE}pleez select your ${LPURPLE}track: ${LGREEN}" # -n to stop carriage return
+  echo -n -e "\n${PURPLE}pleez select your ${LPURPLE}track: ${LGREEN}" # -n to stop carriage return
   read -n 2
-  echo ""
+  echo -e "${BLACK}${CYANBG}"
 
   if [[ $REPLY == "q" ]]
   then
     end=true
+    echo -e "${NC}" # turn off font colors before shutdown
   else
     if [[ $REPLY =~ [0-9] ]] && [[ ${#files[@]} > $REPLY ]]
     # check if num w/regex && check if file array length is larger than number
